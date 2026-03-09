733431232b414d9d6aceab7d1f09487d11dc762b
simsh1031 <sshsim1031@gmail.com>
2026-03-09 19:06:14 +0900

    init: spring boot kotlin planner project

---
+++ src/main/kotlin/com/example/Planner/PlannerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.Planner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlannerApplication
+
+fun main(args: Array<String>) {
+	runApplication<PlannerApplication>(*args)
+}
